8e6ca634ddadf668ad5757b8bb21047f142bac08
jacques <gkounkou@gmail.com>
2024-11-12 20:17:27 -0800

    (chore) Improve the split function

--- logic/string/split-sentence.js
+++ logic/string/split-sentence.js
@@ -2,21 +2,16 @@ var question = [
     {
         id: "split",
         category: "String",
-        placeHolderCpp: `vector<string> split_sentence(const string& sentence) {\n    ...\n}\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n`,
+        placeHolderCpp: `vector<string> splitSentence(const string& sentence) {\n    ...\n}\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n`,
         placeHolderGo: `{\n    ...\n}\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n`,
         difficulty: "Medium",
         question: "Split a sentence into words",
         answerImage: "",
-        answerCpp: `vector<string> split_sentence(const string& sentence) {
+        answerCpp: `vector<string> splitSentence(const string& sentence) {
     stringstream ss(sentence);
-    string word;
-    vector<string> words;
-    
-    while (ss >> word) {
-        words.push_back(word);
-    }
-    
-    return words;
+    return {
+        istream_iterator<string>(ss), {}
+    };
 }`,
         answerGo: ``
     }
