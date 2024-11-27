21b336a756971a0d8dc44e40e3a989960bf31107
Godfrain Jacques <gkounkou@gmail.com>
2024-11-27 11:50:12 -0800

    Update remove-element.js

--- logic/linked-list/remove-element.js
+++ logic/linked-list/remove-element.js
@@ -2,7 +2,7 @@ var question = [
     {
         id: "Remove element",
         category: "Linked List",
-        placeHolderCpp: `struct ListNode {\n    int data;\n    ListNode* next;\n};\n\nListNode* removeElements(ListNode* head, int val) {\n    ...\n}\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n`,
+        placeHolderCpp: `struct ListNode {\n    int data;\n    ListNode* next;\n};\n\nListNode* removeElements(ListNode* head, int t) {\n    ...\n}\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n`,
         placeHolderGo: `func removeElements(head *ListNode, val int) *ListNode {\n    ...\n}\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n`,
         difficulty: "Medium",
         question: "Remove all elements from a linked list that have a specific value.",
@@ -13,14 +13,14 @@ var question = [
     ListNode(int v) : val(v), next(nullptr) {}
 };
 
-ListNode* removeElements(ListNode* head, int v) {
+ListNode* removeElements(ListNode* head, int t) {
     ListNode* dummy = new ListNode(0);
     dummy->next = head;
     ListNode* prev = dummy;
     ListNode* curr = head;
 
     while (curr != nullptr) {
-        if (curr->val == v) {
+        if (curr->val == t) {
             prev->next = curr->next;
         } else {
             prev = curr;
